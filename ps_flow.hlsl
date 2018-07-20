a29c9752cb122c903cbb0cab9c65623f795daf7f
Brady Zhou <brady.zhou@utexas.edu>
2018-07-20 11:21:42 -0500

    refactor and add motion field for camera coords

--- ps_flow.hlsl
+++ ps_flow.hlsl
@@ -7,27 +7,44 @@ SamplerState S : register(s0) {
 	AddressV = BORDER;
 	BorderColor = float4(0, 0, 0, 0);
 };
+
+// Correct the disparity disp_a * (z + disp_b)
 cbuffer disparity_correction {
 	float disp_a = 1.0;
-	float disp_b = 0.0; // Correct the disparity disp_a * (z + disp_b)
+	float disp_b = 0.0;
 };
 
-void main(in float4 p: SV_POSITION, in float2 t : TEX_COORD, out float2 flow : SV_Target0, out float disparity : SV_Target1, out float occlusion: SV_Target2){
-	//float4 f = T.Sample(S, t);
+void main(in float4 p : SV_POSITION, in float2 t : TEX_COORD, out float2 flow : SV_Target0, out float disparity : SV_Target1, out float occlusion: SV_Target2) {
 	uint W, H;
+
 	flow_disp.GetDimensions(W, H);
-	int x = t.x*(W - 1), y = t.y*(H - 1);
+
+	int x = t.x * (W - 1);
+	int y = t.y * (H - 1);
+
 	float4 f = flow_disp.Load(int3(x, y, 0));
 
-	float X = (f.x + 1) * 0.5 * W, Y = (1 - f.y) * 0.5 * H, D = f.z;
+	// Map from ndc coordinates [-1, 1] -> [0, 1].
+	// Note: y coordinate is flipped.
+	float X_0_1 = (f.x + 1) * 0.5;
+	float Y_0_1 = (1 - f.y) * 0.5;
 
-	// Get the prior disparity
-	float DD = prev_disp.Sample(S, float2((f.x + 1) * 0.5, (1 - f.y) * 0.5));
-	disparity = disp_a * (f.w + disp_b);
-	float prev_disparity = disp_a * (f.z + disp_b);
-	occlusion = 1. / prev_disparity - 1. / DD;
-	if (f.z <= 0)
+	// Map to screen coordinates.
+	float X_0_W = X_0_1 * W;
+	float Y_0_H = Y_0_1 * H;
+
+	float D_cur = f.w;
+	float D_prev = f.z;
+
+	float prev_disparity;
+	
+	disparity = disp_a * (D_cur + disp_b);
+	prev_disparity = disp_a * (D_prev + disp_b);
+
+	occlusion = (1. / prev_disparity) - (1. / prev_disp.Sample(S, float2(X_0_1, Y_0_1)));
+
+	if (D_prev <= 0.)
 		flow = 0. / 0.; // NaN
 	else
-		flow = float2(X-x, Y-y) - 0.5;
+		flow = float2(X_0_W - x, Y_0_H - y) - 0.5;
 }
